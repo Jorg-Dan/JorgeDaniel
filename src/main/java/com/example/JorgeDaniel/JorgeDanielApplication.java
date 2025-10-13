a4394277e0af2055b56d9490da4b7bc1477a2073
Jorg-Dan <jorgedaniel.fcamelo@gmail.com>
2025-10-13 18:42:51 -0300

    Ball

---
+++ src/main/java/com/example/JorgeDaniel/JorgeDanielApplication.java
@@ -0,0 +1,13 @@
+package com.example.JorgeDaniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JorgeDanielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JorgeDanielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JorgeDaniel/JorgeDanielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JorgeDaniel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JorgeDanielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
