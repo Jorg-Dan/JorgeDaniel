93c293e48e6835a6a1b09ab7d7a68e36f86fb292
Jorg-Dan <jorgedaniel.fcamelo@gmail.com>
2025-10-13 18:49:41 -0300

    v1

--- src/main/java/com/senac/JorgeDaniel/JorgeDanielApplication.java
+++ src/main/java/com/senac/JorgeDaniel/JorgeDanielApplication.java
@@ -1,4 +1,4 @@
-package com.example.JorgeDaniel;
+package com.senac.JorgeDaniel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/senac/JorgeDaniel/JorgeDanielApplicationTests.java
+++ src/test/java/com/senac/JorgeDaniel/JorgeDanielApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.JorgeDaniel;
+package com.senac.JorgeDaniel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
